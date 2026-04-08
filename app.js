31599055688c2bd8b4282a7df74569244804748f
goldenvertex42 <fabled93@gmail.com>
2026-04-08 12:25:43 -0500

    chore: change app.listen and add start script to package-json

--- app.js
+++ app.js
@@ -88,9 +88,12 @@ app.use('/', authRouter);
 app.use('/files', fileRouter);
 app.use('/folders', folderRouter);
 
-app.listen(3000, (error) => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', (error) => {
   if (error) {
     throw error;
   }
-  console.log("app listening on port 3000!");
+  
+  console.log(`Server running on port ${PORT}`);
 });
